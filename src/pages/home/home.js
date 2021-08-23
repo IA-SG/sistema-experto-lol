ac54ac733f63b68a38c6ff23a0e3a12891455ece
Yeison Garzón T <garzontoroyeison@gmail.com>
2021-08-23 00:29:17 -0500

    pantalla inicial refactorizada

---
+++ src/pages/home/home.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import allChampsImg from "../../assets/all-champs.jpg";
+import "./home.css";
+
+const HomePage = () => {
+  return (
+    <div className="background__container">
+      <h1 className="background__title goo">
+        Sistema Experto - League Of Legends
+      </h1>
+      <svg style={{ visibility: "hidden", position: "absolute" }}>
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+      <Button
+        className="background__button"
+        variant="contained"
+        color="primary"
+        size="large"
+      >
+        Empezar
+      </Button>
+      <img
+        src={allChampsImg}
+        className="background__image"
+        alt="League of Legends - Wallpaper 4K"
+      />
+    </div>
+  );
+};
+
+export default HomePage;
