e78eb54f6d8a1e8d76b7d3a224511463a730853b
Yeison Garzón T <garzontoroyeison@gmail.com>
2021-08-23 00:17:47 -0500

    new entry page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import allChampsImg from "./assets/all-champs.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Holi pendeja, pa que empezemos.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background__container">
+      <h1 className="background__title goo">
+        Sistema Experto - League Of Legends
+      </h1>
+      <svg style={{ visibility: "hidden", position: "absolute" }}>
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+      <Button
+        className="background__button"
+        variant="contained"
+        color="primary"
+        size="large"
+      >
+        Empezar
+      </Button>
+      <img
+        src={allChampsImg}
+        className="background__image"
+        alt="League of Legends - Wallpaper 4K"
+      />
     </div>
   );
 }
