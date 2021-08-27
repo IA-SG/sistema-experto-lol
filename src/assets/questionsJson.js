51dd0f507ac1db22330ee265007114053f85d557
bryanscm <bryan.cardenas4020@gmail.com>
2021-08-27 16:15:40 -0500

    feat: se arregla el codigo

---
+++ src/assets/questionsJson.js
@@ -0,0 +1,83 @@
+export const answersStatic = ["Si", "No"];
+
+export const questionsArray = [
+  {
+    id: 1,
+    question: "¿Cuál es tu linea preferida o que has jugado anteriormente?",
+    answers: ["Top", "Jungla", "Mid", "Adc", "Support"],
+  },
+  {
+    id: 2,
+    question: "¿Qué tipo de daño prefiere AD o AP?",
+    answers: ["AD", "AP"],
+  },
+  {
+    id: 3,
+    question: "¿Jugador Pasivo o Agresivo?",
+    answers: ["Agresivo", "Pasivo"],
+  },
+  {
+    id: 4,
+    question: "¿Te gusta ser el frente del equipo? ",
+    answers: answersStatic,
+  },
+  {
+    id: 5,
+    question: "¿Te gusta jugar en equipo? ",
+    answers: answersStatic,
+    line: "Top",
+    typePlayer: "Agresivo",
+    typeDamage: "AD",
+  },
+  {
+    id: 6,
+    question: "¿Te gusta moverte por el mapa para ayudar a tu equipo?",
+    answers: answersStatic,
+  },
+  {
+    id: 7,
+    question: "¿Normalmente te gusta iniciar peleas?",
+    answers: answersStatic,
+  },
+  {
+    id: 8,
+    question: "¿Te gusta apoyar a tu equipo con daño o control de masas?",
+    answers: ["Daño", "CC"],
+  },
+  {
+    id: 9,
+    question: "¿Te gusta participar en peleas grupales o uno contra uno?",
+    answers: ["Grupales", "Uno vs Uno"],
+  },
+  {
+    id: 10,
+    question: "¿Llegar en sigilo?",
+    answers: answersStatic,
+  },
+  {
+    id: 11,
+    question: "¿Prefieres un campeón estático o con movilidad?",
+    answers: ["Estático", "Movilidad"],
+  },
+  {
+    id: 12,
+    question: "¿Daño explosivo?",
+    answers: answersStatic,
+  },
+  {
+    id: 13,
+    question: "¿Prefieres un campeón frágil o con resistencia?",
+    answers: ["Frágil", "Resistencia"],
+  },
+  {
+    id: 14,
+    question:
+      "¿Defender a tu compañero entorpeciendo al enemigo o dándole protección?",
+    answers: ["Entorpeciendo", "Protección"],
+  },
+  {
+    id: 15,
+    question: "¿Te gusta pokear o ser de combo?",
+    answers: ["Pokear", "Combo"],
+  },
+];
--- src/pages/questions/questions.js
+++ src/pages/questions/questions.js
@@ -1,166 +1,186 @@
-import React, { useState, useEffect } from "react";
-import { firstQuestionsArray, questionsArray } from "../../assets/questions";
-import QuestionComponent from "../../components/answer";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Box from "@material-ui/core/Box";
-import "./questions.css";
+import React, { useState, useEffect } from 'react';
+import {
+    firstQuestionsArray,
+    questionsArray,
+} from '../../assets/questionsJson';
+import QuestionComponent from '../../components/answer';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import './questions.css';
 
 const QuestionsPage = () => {
-  const [index, setIndex] = useState(1);
-  const [newIndex, setNewIndex] = useState(1);
-  const [isFirstQuestion, setIsFirstQuestion] = useState(true);
-  const [line, setLine] = useState("");
-  const [damage, setDamage] = useState("");
-  const [playerType, setPlayerType] = useState("");
-  const [champions, setChampions] = useState([]);
+    const [index, setIndex] = useState(1);
+    const [user, setUser] = useState({
+        line: 0,
+        damage: 0,
+        playerType: 0,
+    });
+    let question = {};
+    const [champions, setChampions] = useState([]);
 
-  const hanldeQuestion = (line, damage, playerType) => {
-    console.log(`${playerType}`);
-    switch (line) {
-      case line[0]:
-        if (damage == damage[0]) {
-          if (playerType == playerType[0]) {
-            setNewIndex(2);
-          } else {
-            setNewIndex(1);
-          }
-        } else {
-          if (playerType == playerType[0]) {
-            setNewIndex(3);
-          } else {
-            setNewIndex(4);
-          }
-        }
-        break;
-      case line[1]:
-        if (damage == damage[0]) {
-          if (playerType == playerType[0]) {
-            setNewIndex(5);
-          } else {
-            setNewIndex(4);
-          }
-        } else {
-          if (playerType == playerType[0]) {
-            setNewIndex(6);
-          } else {
-            setNewIndex(5);
-          }
-        }
-        break;
-      case line[2]:
-        if (damage == damage[0]) {
-          if (playerType == playerType[0]) {
-            setNewIndex(9);
-          } else {
-            //champ response
-          }
-        } else {
-          if (playerType == playerType[0]) {
-            setNewIndex(9);
-          } else {
-            setNewIndex(8);
-          }
-        }
-        break;
-      case line[3]:
-        if (damage == damage[0]) {
-          if (playerType == playerType[0]) {
-          } else {
-          }
-        } else {
-          if (playerType == playerType[0]) {
-          } else {
-          }
-        }
-        break;
-      case line[4]:
-        if (damage == damage[0]) {
-          if (playerType == playerType[0]) {
-            setNewIndex(12);
-          } else {
-            setNewIndex(4);
-          }
-        } else {
-          if (playerType == playerType[0]) {
-            setNewIndex(10);
-          } else {
-            setNewIndex(11);
-          }
-        }
-        break;
-    }
-  };
+    useEffect(() => {
+        question = questionsArray.filter(
+            (question) => question.id === index
+        )[0];
+        console.log('Question ', question);
+    }, [index]);
 
-  const handleFirstQuestions = (questionIndex, responseIndex) => {
-    if (questionIndex === 0) {
-      setLine(firstQuestionsArray[questionIndex].answers[responseIndex]);
-      setIndex(index + 1);
-    } else if (questionIndex === 1) {
-      setDamage(firstQuestionsArray[questionIndex].answers[responseIndex]);
-      setIndex(index + 1);
-    } else if (questionIndex === 2) {
-      setPlayerType(firstQuestionsArray[questionIndex].answers[responseIndex]);
-      setIsFirstQuestion(false);
-    }
-  };
+    const hanldeQuestion = (user, playerType) => {
+        console.log(user);
+        if (user.line === 0) {
+            console.log('Entonces=');
+            if (user.damage === 0) {
+                if (playerType === 0) {
+                    setIndex(5);
+                } else {
+                    setIndex(4);
+                }
+            } else {
+                if (playerType === 0) {
+                    setIndex(6);
+                } else {
+                    setIndex(7);
+                }
+            }
+        } else if (user.line === 1) {
+            if (user.damage === 0) {
+                if (playerType === 0) {
+                    setIndex(8);
+                } else {
+                    setIndex(7);
+                }
+            } else {
+                if (playerType === 0) {
+                    setIndex(9);
+                } else {
+                    setIndex(8);
+                }
+            }
+        } else if (user.line === 2) {
+            if (user.damage === 0) {
+                if (playerType === 0) {
+                    setIndex(12);
+                } else {
+                    //champ response
+                }
+            } else {
+                if (playerType === 0) {
+                    setIndex(12);
+                } else {
+                    setIndex(11);
+                }
+            }
+        } else if (user.line === 3) {
+            if (user.damage === 0) {
+                if (playerType === 0) {
+                } else {
+                }
+            } else {
+                if (playerType === 0) {
+                } else {
+                }
+            }
+        } else if (user.line === 4) {
+            if (user.damage === 0) {
+                if (playerType === 0) {
+                    setIndex(15);
+                } else {
+                    setIndex(7);
+                }
+            } else {
+                if (playerType === 0) {
+                    setIndex(13);
+                } else {
+                    setIndex(14);
+                }
+            }
+        }
+    };
 
-  return (
-    <div className="question__background">
-      <Grid
-        container
-        direction="column"
-        justifyContent="space-evenly"
-        alignItems="center"
-      >
-        <Box boxShadow={3} className="question__container">
-          {isFirstQuestion
-            ? firstQuestionsArray.length !== 0 && (
-                <h2>{firstQuestionsArray[index - 1].question}</h2>
-              )
-            : questionsArray.length !== 0 && (
-                <h2>{questionsArray[newIndex - 1].question}</h2>
-              )}
-        </Box>
-        <Box boxShadow={3} className="question__answer">
-          <Grid
-            container
-            direction="row"
-            justifyContent="space-around"
-            alignItems="center"
-          >
-            {isFirstQuestion
-              ? firstQuestionsArray.length !== 0 &&
-                firstQuestionsArray[index - 1].answers.map((answer, i) => (
-                  <Button
-                    onClick={() => {
-                      handleFirstQuestions(index - 1, i);
-                    }}
-                    variant="outlined"
-                    color="primary"
-                    size="large"
-                  >
-                    {answer}
-                  </Button>
-                ))
-              : questionsArray.length !== 0 &&
-                questionsArray[newIndex - 1].answers.map((answer) => (
-                  <Button
-                    onClick={() => {
-                      hanldeQuestion(line, damage, playerType);
-                    }}
-                    variant="outlined"
-                    color="primary"
-                    size="large"
-                  >
-                    {answer}
-                  </Button>
-                ))}
-          </Grid>
-        </Box>
-      </Grid>
-    </div>
-  );
+    const handleFirstQuestions = (questionIndex, responseIndex) => {
+        console.log('Hi');
+        if (questionIndex === 0) {
+            setUser({ ...user, line: responseIndex });
+            setIndex(index + 1);
+        } else if (questionIndex === 1) {
+            setUser({ ...user, damage: responseIndex });
+            setIndex(index + 1);
+        } else if (questionIndex === 2) {
+            setUser({ ...user, playerType: responseIndex });
+            console.log(user);
+            hanldeQuestion(user, responseIndex);
+        }
+    };
+
+    return (
+        <div className="question__background">
+            <Grid
+                container
+                direction="column"
+                justifyContent="space-evenly"
+                alignItems="center">
+                <Box boxShadow={3} className="question__container">
+                    {false
+                        ? questionsArray.length !== 0 && (
+                              <h2>
+                                  {
+                                      questionsArray.filter(
+                                          (question) => question.id == index
+                                      ).id
+                                  }
+                              </h2>
+                          )
+                        : questionsArray.length !== 0 && (
+                              <>
+                                  <h2>{questionsArray[index - 1].question}</h2>
+                                  <p>{user.playerType}</p>
+                              </>
+                          )}
+                </Box>
+                <Box boxShadow={3} className="question__answer">
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-around"
+                        alignItems="center">
+                        {false
+                            ? firstQuestionsArray.length !== 0 &&
+                              firstQuestionsArray[index - 1].answers.map(
+                                  (answer, i) => (
+                                      <Button
+                                          onClick={() => {
+                                              handleFirstQuestions(index, i);
+                                          }}
+                                          variant="outlined"
+                                          color="primary"
+                                          size="large">
+                                          {answer}
+                                      </Button>
+                                  )
+                              )
+                            : questionsArray.length !== 0 &&
+                              questionsArray[index - 1].answers.map(
+                                  (answer, i) => (
+                                      <Button
+                                          onClick={() => {
+                                              handleFirstQuestions(
+                                                  index - 1,
+                                                  i
+                                              );
+                                          }}
+                                          variant="outlined"
+                                          color="primary"
+                                          size="large">
+                                          {answer}
+                                      </Button>
+                                  )
+                              )}
+                    </Grid>
+                </Box>
+            </Grid>
+        </div>
+    );
 };
 
 export default QuestionsPage;
