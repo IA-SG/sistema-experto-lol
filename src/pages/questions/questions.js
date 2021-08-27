a3d7dc6b3ea055270c29e15e003a60b3fb089f22
Yeison Garzón T <garzontoroyeison@gmail.com>
2021-08-26 22:46:17 -0500

    ui questions changed

--- src/pages/questions/questions.js
+++ src/pages/questions/questions.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import questionsMap from "../../assets/questions";
 import QuestionComponent from "../../components/answer";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import "./questions.css";
 
 const QuestionsPage = () => {
+  const [index, setIndex] = useState(1);
+
+  const handleNext = () => {
+    if (index < questionsMap.length) {
+      setIndex(index + 1);
+    }
+  };
+
   return (
     <div className="question__background">
       <Grid
         container
         direction="column"
-        justifyContent="center"
+        justifyContent="space-evenly"
         alignItems="center"
       >
-        <div
-          style={{ width: "80vw", backgroundColor: "pink" }}
-        >
-          <h2 style={{ textAlign: "center" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </h2>
+        <div className="question__container">
+          {questionsMap.length !== 0 && <h2>{questionsMap[index - 1].question}</h2>}
         </div>
-        <div style={{  paddingTop: '20px', width: "80vw", backgroundColor: "red"}}>
-          <div style={{ width: "100px"}}>2312312</div>
-          <div style={{ width: "100px"}}>2312312</div>
-          <div style={{ width: "100px"}}>2312312</div>
-          <div style={{ width: "100px"}}>2312312</div>
-          <div style={{ width: "100px"}}>2312312</div>
+        <div className="question__answer">
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+          >
+            {questionsMap.length !== 0 &&
+              questionsMap[index - 1].answers.map((answer) => (
+                <Button onClick={handleNext} variant="outlined" color="primary">
+                  {answer}
+                </Button>
+              ))}
+          </Grid>
         </div>
       </Grid>
     </div>
