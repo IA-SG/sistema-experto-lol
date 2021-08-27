080d11d7e6b0ae0049495bef6e9207aaaa0a1572
bryanscm <bryan.cardenas4020@gmail.com>
2021-08-27 17:50:31 -0500

    feat: se agrega los campeones

---
+++ src/assets/championsJson.js
@@ -0,0 +1,174 @@
+export const championArray = [
+    {
+        id: 1,
+        name: 'SHEN',
+    },
+    {
+        id: 2,
+        name: 'KLED',
+    },
+    {
+        id: 3,
+        name: 'DARIUS',
+    },
+    {
+        id: 4,
+        name: 'GAREN',
+    },
+    {
+        id: 5,
+        name: 'NASUS',
+    },
+    {
+        id: 6,
+        name: 'DR.MUNDO',
+    },
+    {
+        id: 7,
+        name: 'JAX',
+    },
+    {
+        id: 8,
+        name: 'SINGED',
+    },
+    {
+        id: 9,
+        name: 'VOLIBER',
+    },
+    {
+        id: 10,
+        name: 'SYLAS',
+    },
+    {
+        id: 11,
+        name: 'KENNEN',
+    },
+    {
+        id: 12,
+        name: 'MALPHITE',
+    },
+    {
+        id: 13,
+        name: 'HEIMERDINGER',
+    },
+    {
+        id: 14,
+        name: 'CHOGATH',
+    },
+    {
+        id: 15,
+        name: 'HECARIM',
+    },
+    { 
+        id: 16,
+        name: 'JARVANIV',
+    },
+    {
+        id: 17,
+        name: 'VI',
+    },
+    {
+        id: 18,
+        name: 'KAYN',
+    },
+    {
+        id: 19,
+        name: 'WARWICK',
+    },
+    {
+        id: 20,
+        name: 'TRUNDLE',
+    },
+    {
+        id: 21,
+        name: 'UDIR',
+    },
+    {
+        id: 22,
+        name: 'EKKO',
+    },
+    {
+        id: 23,
+        name: 'DIANA',
+    },
+    {
+        id: 24,
+        name: 'ZAC',
+    },
+    {
+        id: 25,
+        name: 'Amumu',
+    },
+    {
+        id: 26,
+        name: 'SHYVANA',
+    },
+    {
+        id: 27,
+        name: 'LUCIA',
+    },
+    {
+        id: 28,
+        name: 'TALON',
+    },
+    {
+        id: 29,
+        name: 'VIEGO',
+    },
+    {
+        id: 30,
+        name: 'YASUO',
+    },
+    {
+        id: 31,
+        name: 'CORKI',
+    },
+    {
+        id: 32,
+        name: 'ANNIE',
+    },
+    {
+        id: 33,
+        name: 'NEKKO',
+    },
+    {
+        id: 34,
+        name: 'GALIO',
+    },
+    {
+        id: 35,
+        name: 'ANIVIA',
+    },
+    {
+        id: 36,
+        name: 'PANTHEON',
+    },
+    {
+        id: 37,
+        name: 'PYKE',
+    },
+    {
+        id: 38,
+        name: 'TRESH',
+    },
+    {
+        id: 39,
+        name: 'BRAUM',
+    },
+    {
+        id: 40,
+        name: 'BLIZCRANK',
+    },
+    {
+        id: 41,
+        name: 'NAUTILIUS',
+    },
+    {
+        id: 42,
+        name: 'KARMA',
+    },
+    {
+        id: 43,
+        name: 'TARIC',
+    },
+];
--- src/pages/questions/questions.js
+++ src/pages/questions/questions.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import {
-    questionsArray,
-} from '../../assets/questionsJson';
+import { questionsArray } from '../../assets/questionsJson';
+import { championArray } from '../../assets/championsJson';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
@@ -13,12 +12,14 @@ const QuestionsPage = () => {
         line: 0,
         damage: 0,
         playerType: 0,
+        desglozar: false,
     });
     const [champions, setChampions] = useState([]);
+    const [isChampions, setIsChampions] = useState(false);
 
     const hanldeQuestion = (user, playerType) => {
+        console.log('Hola');
         if (user.line === 0) {
-            console.log('Entonces=');
             if (user.damage === 0) {
                 if (playerType === 0) {
                     setIndex(5);
@@ -85,10 +86,92 @@ const QuestionsPage = () => {
                 }
             }
         }
+        setUser({ ...user, desglozar: true });
+    };
+
+    const handleQuestionNext = (questionIndex, reponseIndex) => {
+        console.log(questionIndex, reponseIndex);
+        if (questionIndex === 5) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[0], championArray[1]]);
+                
+            } else {
+                setChampions([championArray[2], championArray[3]]);
+                
+            }
+        } else if (questionIndex === 4) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[4], championArray[5]]);
+            } else {
+                setChampions([championArray[6]]);
+            }
+        } else if (questionIndex === 6) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[7], championArray[8]]);
+            } else {
+                setChampions([championArray[9], championArray[10]]);
+            }
+        } else if (questionIndex === 7) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[11]]);
+            } else {
+                setChampions([championArray[12], championArray[13]]);
+            }   
+        } else if (questionIndex === 8) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[14], championArray[15], championArray[16]]);
+            } else {
+                setChampions([championArray[17]]);
+            }
+        } else if (questionIndex === 9) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[18], championArray[19]]);
+            } else {
+                setChampions([championArray[20]]);
+            }
+        //Peleas grupales o uno contra uno
+        } else if (questionIndex === 10) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[21]]);
+            } else {
+                setChampions([championArray[22]]);
+            }
+        // Control de masas jungle
+        } else if (questionIndex === 11) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[23]]);
+            } else {
+                setChampions([championArray[25]]);
+            }
+        } else if (questionIndex === 13) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[26]]);
+            } else {
+                setChampions([championArray[29]]);
+            }
+        } else if (questionIndex === 15) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[31]]);
+            } else {
+                setChampions([championArray[32]]);
+            }
+        } else if (questionIndex === 16) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[33]]);
+            } else {
+                setChampions([championArray[34]]);
+            }
+        } else if (questionIndex === 17) {
+            if (reponseIndex === 0) {
+                setChampions([championArray[35]]);
+            } else {
+                setChampions([championArray[36]]);
+            }
+        }
+        setIsChampions(true);
     };
 
     const handleFirstQuestions = (questionIndex, responseIndex) => {
-        console.log('Hi');
         if (questionIndex === 0) {
             setUser({ ...user, line: responseIndex });
             setIndex(index + 1);
@@ -104,41 +187,64 @@ const QuestionsPage = () => {
 
     return (
         <div className="question__background">
-            <Grid
-                container
-                direction="column"
-                justifyContent="space-evenly"
-                alignItems="center">
-                <Box boxShadow={3} className="question__container">
-                    {questionsArray.length !== 0 && (
-                        <>
-                            <h2>{questionsArray[index - 1].question}</h2>
-                        </>
-                    )}
-                </Box>
-                <Box boxShadow={3} className="question__answer">
-                    <Grid
-                        container
-                        direction="row"
-                        justifyContent="space-around"
-                        alignItems="center">
-                        {questionsArray.length !== 0 &&
-                            questionsArray[index - 1].answers.map(
-                                (answer, i) => (
-                                    <Button
-                                        onClick={() => {
-                                            handleFirstQuestions(index - 1, i);
-                                        }}
-                                        variant="outlined"
-                                        color="primary"
-                                        size="large">
-                                        {answer}
-                                    </Button>
-                                )
-                            )}
-                    </Grid>
-                </Box>
-            </Grid>
+            {!isChampions ? (
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="space-evenly"
+                    alignItems="center">
+                    <Box boxShadow={3} className="question__container">
+                        {questionsArray.length !== 0 && (
+                            <>
+                                <h2>{questionsArray[index - 1].question}</h2>
+                            </>
+                        )}
+                    </Box>
+                    <Box boxShadow={3} className="question__answer">
+                        <Grid
+                            container
+                            direction="row"
+                            justifyContent="space-around"
+                            alignItems="center">
+                            {questionsArray.length !== 0 &&
+                                questionsArray[index - 1].answers.map(
+                                    (answer, i) => (
+                                        <Button
+                                            onClick={() => {
+                                                !user.desglozar
+                                                    ? handleFirstQuestions(
+                                                          index - 1,
+                                                          i
+                                                      )
+                                                    : handleQuestionNext(
+                                                          index,
+                                                          i
+                                                      );
+                                            }}
+                                            variant="outlined"
+                                            color="primary"
+                                            size="large">
+                                            {answer}
+                                        </Button>
+                                    )
+                                )}
+                        </Grid>
+                    </Box>
+                </Grid>
+            ) : (
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="center">
+                    <Box boxShadow={3} className="question__answer">
+                        {champions.length !== 0 &&
+                            champions.map((champion, i) => (
+                                <h2>{champion.name}</h2>
+                            ))}
+                    </Box>
+                </Grid>
+            )}
         </div>
     );
 };
